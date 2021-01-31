6c75f07fbdc8cc69291c4d98a9f0d9304d6b0e16
Charlie Scherer <charlie@tildesrc.com>
2021-01-30 19:51:51 -0600

    Download history to a csv

--- shell/src/PuttHistory.tsx
+++ shell/src/PuttHistory.tsx
@@ -1,16 +1,16 @@
 import {IconButton, Table, TableBody, TableCell, TableHead, TableRow} from '@material-ui/core';
-import {Delete} from '@material-ui/icons';
+import {CloudDownload, Delete} from '@material-ui/icons';
 import {get, set} from 'idb-keyval';
-import React, { useState } from 'react';
+import React, { createRef, useState, useEffect } from 'react';
 import { FormatPercentage } from './common';
 import { repSetType } from './types';
+import { createObjectCsvStringifier } from 'csv-writer';
 
 export default function PuttHistory() {
   let [repSets, setRepSets] = useState<repSetType[]>();
 
   async function loadRepSets() {
     setRepSets(await get('repSets'));
-    console.log(await get('repSets'));
   }
   if (!repSets) loadRepSets();
 
@@ -24,11 +24,30 @@ export default function PuttHistory() {
     setRepSets(repSets);
   }
 
+  let downloadLinkRef = createRef<HTMLAnchorElement>();
+
+  useEffect(() => {
+    if (!repSets) return;
+    let csvWriter = createObjectCsvStringifier({
+      header: [
+        { id: 'updatedAt', title: 'Last Updated' },
+        { id: 'hits', title: 'Hits' },
+        { id: 'misses', title: 'Misses' },
+      ]
+    });
+    let payload = [
+      csvWriter.getHeaderString(), csvWriter.stringifyRecords(repSets!.filter((__, idx) => idx !== 0))
+    ].join('')
+    downloadLinkRef!.current!.href = `data:text/plain;charset=utf-8,${encodeURIComponent(payload)}`
+  }, [repSets, downloadLinkRef])
+
   return (
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell width={1}/>
+          <TableCell width={1}>
+            <IconButton><a href='#' ref={downloadLinkRef} download='putts.csv'><CloudDownload /></a></IconButton>
+          </TableCell>
           <TableCell>
             Last Updated
           </TableCell>
